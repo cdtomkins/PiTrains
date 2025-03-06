ddd204d77588e1317c48fe57b9509e58fd598c2b
Chris Tomkins <cdtomkins@gmail.com>
2025-03-06 19:48:11 +0000

    Improved terminal output and removed syslog

--- PiTrains.py
+++ PiTrains.py
@@ -5,9 +5,9 @@
 # Expects DESTINATION_CRS_CODE environment variable (e.g. "BTN" - the destination station that we're interested in)
 
 import os
-import syslog
 from datetime import datetime, timedelta
 from nredarwin.webservice import DarwinLdbSession
+from pprint import pprint
 try:
     from blinkt import set_pixel, set_clear_on_exit, show
     GOT_BLINKT=True
@@ -22,18 +22,27 @@ DARWINSESSION = DarwinLdbSession(wsdl='https://lite.realtime.nationalrail.co.uk/
 DEPARTUREBOARD = DARWINSESSION.get_station_board(os.environ["DEPARTURE_CRS_CODE"],destination_crs=os.environ["DESTINATION_CRS_CODE"])
 
 # Initialise lists
-ServiceStatus = []
-Services = []
+HumanReadableServices = []
+ParsedServices = []
 LEDs = [0]*AVAILABLELEDCOUNT
+
+# White=5=imminent/just left, Green=4=on time, yellow=3=late, blue=2=unspecified delay, red=1=cancelled, black=0=none
+COLOUR_LOOKUP_TABLE = [
+    "Black",
+    "Red",
+    "Blue",
+    "Yellow",
+    "Green",
+    "White"
+]
 
 train_services = DEPARTUREBOARD.train_services
 
-# Populate ServiceStatus and Services.
-# White=5=imminent/just left, Green=4=on time, yellow=3=late, blue=2=unspecified delay, red=1=cancelled, black=0=none
+# Populate HumanReadableServices and ParsedServices.
 # We also check and deal with trains that wrap over midnight
 for service in train_services:
 
-    ServiceStatus.append((service.std, service.etd))
+    HumanReadableServices.append({"ScheduledTime": service.std, "ExpectedTime": service.etd, "OperatorName": service.operator_name})
     STD = datetime.strptime(CURRENTYMD+service.std, '%Y-%m-%d%H:%M')
 
     if(service.etd=="On time"):
@@ -41,25 +50,25 @@ for service in train_services:
             STD += timedelta(1)
         TimeDelta = STD-CURRENTTIME
         if(int(TimeDelta.total_seconds()/60/5)<=AVAILABLELEDCOUNT-1):
-            Services.append((int(TimeDelta.total_seconds()/60/5),4))
+            ParsedServices.append((int(TimeDelta.total_seconds()/60/5),4,COLOUR_LOOKUP_TABLE[4]))
         elif(int(TimeDelta.total_seconds()/60/5)>250):
-            Services.append((0,5))
+            ParsedServices.append((0,5,COLOUR_LOOKUP_TABLE[5]))
 
     elif(service.etd=="Delayed"):
         if STD < CURRENTTIME:
             STD += timedelta(1)
         TimeDelta = STD-CURRENTTIME
         if(int(TimeDelta.total_seconds()/60/5)<=AVAILABLELEDCOUNT-1):
-            Services.append((int(TimeDelta.total_seconds()/60/5),2))
+            ParsedServices.append((int(TimeDelta.total_seconds()/60/5),2,COLOUR_LOOKUP_TABLE[2]))
         elif(int(TimeDelta.total_seconds()/60/5)>250):
-            Services.append((0,5))
+            ParsedServices.append((0,5,COLOUR_LOOKUP_TABLE[5]))
 
     elif(service.etd=="Cancelled"):
         if STD < CURRENTTIME:
             STD += timedelta(1)
         TimeDelta = STD-CURRENTTIME
         if(int(TimeDelta.total_seconds()/60/5)<=AVAILABLELEDCOUNT-1):
-            Services.append((int(TimeDelta.total_seconds()/60/5),1))
+            ParsedServices.append((int(TimeDelta.total_seconds()/60/5),1,COLOUR_LOOKUP_TABLE[1]))
 
     else:
         ETD = datetime.strptime(CURRENTYMD+service.etd, '%Y-%m-%d%H:%M')
@@ -67,22 +76,15 @@ for service in train_services:
             ETD += timedelta(1)
         TimeDelta = ETD-CURRENTTIME
         if(int(TimeDelta.total_seconds()/60/5)<=AVAILABLELEDCOUNT-1):
-            Services.append((int(TimeDelta.total_seconds()/60/5),3))
+            ParsedServices.append((int(TimeDelta.total_seconds()/60/5),3,COLOUR_LOOKUP_TABLE[3]))
         elif(int(TimeDelta.total_seconds()/60/5)>250):
-            Services.append((0,5))
+            ParsedServices.append((0,5,COLOUR_LOOKUP_TABLE[5]))
 
-# Syslog ServiceStatus and Services
-syslog.syslog(str(ServiceStatus))
-syslog.syslog(str(Services))
-
-# Dedupe Services to create LEDs. If one time slot contains more than one
+# Dedupe ParsedServices to create LEDs. If one time slot contains more than one
 # train, the "best" LED state is set - White>Green>Yellow>Blue>Red>Black
-for Service in Services:
+for Service in ParsedServices:
         if(Service[1]>LEDs[Service[0]]):
             LEDs[Service[0]]=Service[1]
-
-# Syslog LEDs
-syslog.syslog(str(LEDs))
 
 # Set Blinkt! LEDs, if we have one
 if GOT_BLINKT:
@@ -97,11 +99,15 @@ if GOT_BLINKT:
             set_pixel(AVAILABLELEDCOUNT-1-idx,0,1,0)
         elif(LEDToSet==1):
             set_pixel(AVAILABLELEDCOUNT-1-idx,1,0,0)
-
     set_clear_on_exit(False)
     show()
-# Otherwise, print them
+# Otherwise, pprint them
 else:
-    print(str(ServiceStatus))
-    print(str(Services))
-    print(str(LEDs))
+    DATA_TO_OUTPUT = {
+        "DEPARTURE_CRS_CODE": os.environ["DEPARTURE_CRS_CODE"],
+        "DESTINATION_CRS_CODE": os.environ["DESTINATION_CRS_CODE"],
+        "HumanReadableServices": HumanReadableServices,
+        "ParsedServices": ParsedServices,
+        "LEDs": LEDs
+    }
+    pprint(DATA_TO_OUTPUT)
